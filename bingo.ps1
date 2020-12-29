8b079f03f56b04d4a31f4cdb5c964347f7941f21
Ting3l <henning@gehebajo.de>
2020-12-29 13:25:14 +0100

    Get-Content // Add comments
    Changed manual array to Get-Content from file
    
    Added comments

--- bingo.ps1
+++ bingo.ps1
@@ -1,36 +1,10 @@
-$array = @(
-    'Kunde trägt die Maske nicht richtig'
-    'Kunde nimmt zum Sprechen die Maske ab'
-    'Kunde hat die Brille beschlagen'
-    'Kunde beschwert sich über Maskentragen'
-    'Kunde fragt was die Zählkarten sollen'
-    'Kunde hinterfragt Ausagen / "Das habe ich nicht hier." "Haben sie das nicht hier?"'
-    '"Wie lautet Ihr Nachname?" "Ich bin schon im ihrem System"'
-    '"Haben Sie das auch nochmal neu eingepackt da?"'
-    '"Ich hab da mal eine kurze Frage." -> Kunde erzählt seine Lebensgeschichte'
-    'Kunde hat einen süßen Hund dabei.'
-    '"Das kostet im Internet aber nur..."'
-    '"Für welches alter soll es sein?" -> Kunde zeigt nur auf ein Kind'
-    'Kunde spricht eine fremde Sprache'
-    '"Früher hatten Sie das aber mal."'
-    '"Gibt es das auch als Taschenbuch?"'
-    '"Packen Sie das auch ein?"'
-    '"Haben Sie hier eine Toilette?"'
-    '"Haben Sie davon noch mehr auf Lager?"'
-    'Kunde will am Infoschalter (bar) bezahlen'
-    'frisch geraucht/ Deo vergessen'
-    '"Nein ich weiß nicht was der Beschenkte mag"'
-    '"Ich hab online gesehen, dass Sie das haben."'
-    'Kunde hat wärend dem Gespräch Kopfhörer drin / telefoniert'
-    '"Ist das bis Weihnachten da?"'
-    '"Ist das hier Etage XY?"'
-    '"Haben Sie auch fremdsprachige Bücher?"'
-    '"Ich brauch das aber heute!" -> Erwartungsvoller Blick'
-    'Kunde unterbricht ein anderes Gespräch'
-    'Anklagende Fragestellung Bsp. "Das haben Sie bestimmt nicht hier!"'
-)
+﻿# Get content for Bingo from file
+$array = Get-Content -Encoding UTF8 "C:\Users\User\Desktop\Lines.txt"
+
+# How many different Bingo-sheets should be generated
 $repeats = 25
 
+# Define HTML-snippets
 $top = '<style>
 th, td {
 padding: 10px;
@@ -41,34 +15,46 @@ width: 20%;
 </style>
 <body style="color: black;font-size: 20px;font-family: Verdana,Arial, Helvetica, sans-serif;">'
 $tableTop = '<table style="border-collapse: collapse;page-break-before: always;">'
-$tableTop2 = '<table style="border-collapse: collapse;">'
 $tableBottom = '</table>'
 $bottom = '</body>'
 
-$html = ""
-$html += $top
+# Begin generation
+$html = $top
 $html += "`n"
+
+# Begin iteration for sheets
 for ($i = 0; $i -lt $repeats; $i++){
+    # Set array for lines of current sheet
     $arrayTMP = $array
+
     $html += $tableTop
+
+    # Begin iteration for rows of current sheet
     for ($j = 0; $j -lt 5; $j++){
         $html += "<tr>"
         $html += "`n"
+
+        # Begin iteration for single fields of current row
         for ($k = 0; $k -lt 5; $k++){
+            
+            # If middle of sheet -> Free
             if ($k -eq 2 -and $j -eq 2){
                 $html += "<td>"
-                $html += "Frei"
+                $html += "Frei / Free"
                 $html += "</td>"
                 $html += "`n"
             }
             else{
+                # Get random line from array
                 $random = Get-Random -Maximum ($arrayTMP.Count) -Minimum 0
-            
+                
+                # Add line to HTML
                 $html += "<td>"
                 $html += $arrayTMP[$random]
                 $html += "</td>"
                 $html += "`n"
 
+                # Remove line from array
                 $arrayTMP = @($arrayTMP | where{$_ -notlike $arrayTMP[$random]})
             }
         }
@@ -79,5 +65,5 @@ for ($i = 0; $i -lt $repeats; $i++){
 }
 $html += $bottom
 
-#$html
+# Export finished sheets
 New-Item -Path "C:\Users\User\Desktop" -Name Bingo.html -ItemType File -Value $html -Force
